17d07f2a79778d4892872e1d371e44a103ffdb88
Oron Sultan <OronS@eladsystems.co.il>
2019-02-13 17:33:57 +0200

    .

--- src/app/exam/exam.component.ts
+++ src/app/exam/exam.component.ts
@@ -7,24 +7,41 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 })
 export class ExamComponent implements OnInit {
   grades: Array<number>;
+  warningMsg: string;
   @ViewChild("txtGrade") gradeField: ElementRef;
   grade: number;
   constructor() { 
+    this.grade = 120;
+    this.warningMsg = '';
     this.grades = new Array<number>();
   }
 
   ngOnInit() {
+    this.eventKeuUp();
   }
   addGrade(){
-    if (this.grade != null && this.grade <= 100) {
+    if (this.grade >= 0 && this.grade <= 100) {
+      this.warningMsg = '';
       this.grades.push(this.grade);
       this.grade = null; 
       this.gradeField.nativeElement.focus();
+    }
+    else{
+      this.warningMsg = "Grade must be between 0 and 100";
     }
   }
   eventHandler(keyCode) {
     if (keyCode == 13) {
      this.addGrade(); 
+      }
     }
- } 
+    eventKeuUp(){
+      console.log(this.grade);
+      if (this.grade > 100) {
+        this.warningMsg = "Grade must be between 0 and 100";
+      }
+      else{
+        this.warningMsg = '';
+      }
+    }
 }
